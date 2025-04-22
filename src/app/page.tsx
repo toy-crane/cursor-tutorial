6bf36f1f3ecb0af6f0973e4247c7999911492a59
toy-crane <alwaysfun2183@gmail.com>
2025-04-22 11:40:44 +0900

    feat: shadcn 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-4xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
